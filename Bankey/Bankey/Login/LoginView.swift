2d298ae28caa6bd69f7c3c413edb9bd20728cce1
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-09-23 07:19:31 -0600

    Added username textfield

--- Bankey/Bankey/Login/LoginView.swift
+++ Bankey/Bankey/Login/LoginView.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class LoginView: UIView {
     
+    let usernameTextField = UITextField()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -20,10 +22,6 @@ class LoginView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    override var intrinsicContentSize: CGSize {
-        return CGSize(width: 200, height: 200)
-    }
 }
 
 extension LoginView {
@@ -31,9 +29,52 @@ extension LoginView {
     func style() {
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .orange
+        
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        usernameTextField.placeholder = "Username"
+        usernameTextField.backgroundColor = .systemFill
+        usernameTextField.delegate = self
     }
     
     func layout() {
+        addSubview(usernameTextField)
         
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: topAnchor),
+            usernameTextField.leadingAnchor.constraint(equalTo: leadingAnchor),
+            usernameTextField.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+}
+
+// MARK: - UITextFieldDelegate
+
+extension LoginView: UITextFieldDelegate {
+    
+    @objc func searchPressed(_ sender: UIButton) {
+        usernameTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Enter username here..."
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if let username = usernameTextField.text {
+            print(username)
+        }
+        
+        usernameTextField.text = ""
     }
 }
