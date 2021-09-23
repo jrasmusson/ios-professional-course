85590091e3a813aeb55886feda96978877b6e0f1
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-09-23 06:59:39 -0600

    cleanup

---
+++ Bankey/Bankey/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by jrasmusson on 2021-09-23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
