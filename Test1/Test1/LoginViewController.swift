e622789b38dca31bc61424fee33fd43ea4f6080d
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-09-24 14:23:22 -0600

    cleanup

---
+++ Test1/Test1/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Test1
+//
+//  Created by jrasmusson on 2021-09-24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    let loginView = LoginView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension LoginViewController {
+    private func style() {
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
+
