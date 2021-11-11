0a9d4d19e8e4fb20801056275142b2cdcb020044
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-11-11 14:02:17 -0700

    Update OnboardingContainer Part I

--- Bankey/2-Onboarding-Part-I/Bankey/Onboarding/OnboardingContainerViewController.swift
+++ Bankey/2-Onboarding-Part-I/Bankey/Onboarding/OnboardingContainerViewController.swift
@@ -2,7 +2,7 @@
 //  OnboardingContainerViewController.swift
 //  Bankey
 //
-//  Created by jrasmusson on 2021-09-28.
+//  Created by jrasmusson on 2021-10-02.
 //
 
 import UIKit
@@ -10,22 +10,9 @@ import UIKit
 class OnboardingContainerViewController: UIViewController {
 
     let pageViewController: UIPageViewController
-    
     var pages = [UIViewController]()
-    
-    var currentVC: UIViewController {
-        didSet {
-            guard let index = pages.firstIndex(of: currentVC) else { return }
-            nextButton.isHidden = index == pages.count - 1 // hide if on last page
-            backButton.isHidden = index == 0
-            doneButton.isHidden = !(index == pages.count - 1) // show if on last page
-        }
-    }
-    
-    let nextButton = UIButton(type: .system)
-    let backButton = UIButton(type: .system)
+    var currentVC: UIViewController
     let closeButton = UIButton(type: .system)
-    let doneButton = UIButton(type: .system)
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
@@ -33,7 +20,7 @@ class OnboardingContainerViewController: UIViewController {
         let page1 = OnboardingViewController(heroImageName: "delorean", titleText: "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in the 80s.")
         let page2 = OnboardingViewController(heroImageName: "world", titleText: "Move your money around the world quickly and securely.")
         let page3 = OnboardingViewController(heroImageName: "thumbs", titleText: "Learn more at www.bankey.com.")
-
+        
         pages.append(page1)
         pages.append(page2)
         pages.append(page3)
@@ -77,51 +64,18 @@ class OnboardingContainerViewController: UIViewController {
     }
     
     private func style() {
-        nextButton.translatesAutoresizingMaskIntoConstraints = false
-        nextButton.setTitle("Next", for: [])
-        nextButton.addTarget(self, action: #selector(nextTapped), for: .primaryActionTriggered)
-        
-        backButton.translatesAutoresizingMaskIntoConstraints = false
-        backButton.setTitle("Back", for: [])
-        backButton.addTarget(self, action: #selector(backTapped), for: .primaryActionTriggered)
-        
         closeButton.translatesAutoresizingMaskIntoConstraints = false
         closeButton.setTitle("Close", for: [])
         closeButton.addTarget(self, action: #selector(closeTapped), for: .primaryActionTriggered)
-        
-        doneButton.translatesAutoresizingMaskIntoConstraints = false
-        doneButton.setTitle("Done", for: [])
-        doneButton.addTarget(self, action: #selector(doneTapped), for: .primaryActionTriggered)
+
+        view.addSubview(closeButton)
     }
     
     private func layout() {
-        view.addSubview(nextButton)
-        view.addSubview(backButton)
-        view.addSubview(closeButton)
-        view.addSubview(doneButton)
-
-        // Next
-        NSLayoutConstraint.activate([
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: nextButton.trailingAnchor, multiplier: 2),
-            view.bottomAnchor.constraint(equalToSystemSpacingBelow: nextButton.bottomAnchor, multiplier: 4)
-        ])
-        
-        // Back
-        NSLayoutConstraint.activate([
-            backButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
-            view.bottomAnchor.constraint(equalToSystemSpacingBelow: backButton.bottomAnchor, multiplier: 4)
-        ])
-        
         // Close
         NSLayoutConstraint.activate([
             closeButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
             closeButton.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 2)
-        ])
-        
-        // Close
-        NSLayoutConstraint.activate([
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: doneButton.trailingAnchor, multiplier: 2),
-            view.bottomAnchor.constraint(equalToSystemSpacingBelow: doneButton.bottomAnchor, multiplier: 4)
         ])
     }
 }
@@ -139,13 +93,13 @@ extension OnboardingContainerViewController: UIPageViewControllerDataSource {
 
     private func getPreviousViewController(from viewController: UIViewController) -> UIViewController? {
         guard let index = pages.firstIndex(of: viewController), index - 1 >= 0 else { return nil }
-        self.currentVC = pages[index - 1]
+        currentVC = pages[index - 1]
         return pages[index - 1]
     }
 
     private func getNextViewController(from viewController: UIViewController) -> UIViewController? {
         guard let index = pages.firstIndex(of: viewController), index + 1 < pages.count else { return nil }
-        self.currentVC = pages[index + 1]
+        currentVC = pages[index + 1]
         return pages[index + 1]
     }
 
@@ -158,23 +112,9 @@ extension OnboardingContainerViewController: UIPageViewControllerDataSource {
     }
 }
 
-// MARK: Actions
+// MARK: - Actions
 extension OnboardingContainerViewController {
-    @objc func nextTapped(_ sender: UIButton) {
-        guard let nextVC = getNextViewController(from: currentVC) else { return }
-        pageViewController.setViewControllers([nextVC], direction: .forward, animated: true, completion: nil)
-    }
-    
-    @objc func backTapped(_ sender: UIButton) {
-        guard let previousVC = getPreviousViewController(from: currentVC) else { return }
-        pageViewController.setViewControllers([previousVC], direction: .reverse, animated: true, completion: nil)
-    }
-    
     @objc func closeTapped(_ sender: UIButton) {
-        // TODO
-    }
-    
-    @objc func doneTapped(_ sender: UIButton) {
         // TODO
     }
 }
