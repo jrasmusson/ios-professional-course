2d9e4eb55d90a54a50fc61c3d03ac6524c241be7
Jonathan Rasmusson <40837677+jrasmusson@users.noreply.github.com>
2023-06-11 07:53:08 -0700

    update: add comment regarding setStatusBar functionality

--- Bankey/99-Final-Solution/Bankey/Utils/UIViewController+Utils.swift
+++ Bankey/99-Final-Solution/Bankey/Utils/UIViewController+Utils.swift
@@ -8,14 +8,25 @@
 import UIKit
 
 extension UIViewController {
+    
+    // This one styles both the status bar and the navigation bar.
     func setStatusBar() {
         let navBarAppearance = UINavigationBarAppearance()
         navBarAppearance.configureWithTransparentBackground() // to hide Navigation Bar Line also
         navBarAppearance.backgroundColor = appColor
         UINavigationBar.appearance().standardAppearance = navBarAppearance
         UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
-
     }
+
+    // Deprecated - this one styles just the status bar.
+//    func setStatusBar() {
+//        let statusBarSize = UIApplication.shared.statusBarFrame.size // deprecated but OK
+//        let frame = CGRect(origin: .zero, size: statusBarSize)
+//        let statusbarView = UIView(frame: frame)
+//         
+//        statusbarView.backgroundColor = .systemPink
+//        view.addSubview(statusbarView)
+//    }
     
     func setTabBarImage(imageName: String, title: String) {
         let configuration = UIImage.SymbolConfiguration(scale: .large)
